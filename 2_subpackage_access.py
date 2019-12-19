7d17e482fd628ed267dfa5aafc1d9c03835151b8
naganjaneyulukode <58967704+naganjaneyulukode@users.noreply.github.com>
2019-12-19 12:43:47 +0530

    pythonfile

--- 2_subpackage_access.py
+++ 2_subpackage_access.py
@@ -1,14 +1,14 @@
-Importing subpackage
-import pckg.spkg.sm1
-import pckg.spkg.sm2
-Accessing the code pckg.spkg.sm1.sf1m1()
-pckg.spkg.sm2.sf1m2()
+#Importing subpackage
+#import pckg.spkg.sm1
+##import pckg.spkg.sm2
+#Accessing the code pckg.spkg.sm1.sf1m1()
+#pckg.spkg.sm2.sf1m2()
 
-Length of import is more. So we can avoid this by bringing needed 
-functions/code to packages namespace. For this we use
-special file "__init__.py". Instead of importing the code
-here we import this in the __init__.py making it accesssible from 
-packages namespace as below. See __init__.py code to understand how
+#Length of import is more. So we can avoid this by bringing needed 
+#functions/code to packages namespace. For this we use
+#special file "__init__.py". Instead of importing the code
+#here we import this in the __init__.py making it accesssible from 
+#packages namespace as below. See __init__.py code to understand how
 
 # from pckg import f1m1
 # from pckg import f1m2
